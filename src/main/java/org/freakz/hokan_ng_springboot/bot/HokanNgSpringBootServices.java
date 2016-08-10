b2d04690200c8d86bb9d715c5bd5ffe9e76244cd
Petri Airio <petri.j.airio@gmail.com>
2016-08-10 11:14:46 +0100

    Test
    Identification: -
    Change-Id: I38cc8ed2325e56134c4c3fe2cc40f6e5051c4e8a

--- src/main/java/org/freakz/hokan_ng_springboot/bot/HokanNgSpringBootServices.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/HokanNgSpringBootServices.java
@@ -6,19 +6,29 @@ import org.freakz.hokan_ng_springboot.bot.enums.CommandLineArgs;
 import org.freakz.hokan_ng_springboot.bot.util.CommandLineArgsParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.jms.ConnectionFactory;
 import java.util.Map;
 
-@Configuration
-@ComponentScan({"org.freakz.hokan_ng_springboot.bot"})
-@EnableJpaRepositories({"org.freakz.hokan_ng_springboot.bot"})
-@EnableAutoConfiguration
+//@Configuration
+//@ComponentScan({"org.freakz.hokan_ng_springboot.bot"})
+//@EnableJpaRepositories({"org.freakz.hokan_ng_springboot.bot"})
+//@ComponentScan
+//@EnableJpaRepositories
+//@EnableAutoConfiguration
+//
+
+
+@SpringBootApplication
+@EnableJms
+@EnableJpaRepositories
 @EnableTransactionManagement
 @Slf4j
 public class HokanNgSpringBootServices {
@@ -27,7 +37,9 @@ public class HokanNgSpringBootServices {
 
   @Bean
   public ConnectionFactory connectionFactory() {
-    return new ActiveMQConnectionFactory(JMS_BROKER_URL);
+    ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(JMS_BROKER_URL);
+    activeMQConnectionFactory.setTrustAllPackages(true);
+    return activeMQConnectionFactory;
   }
 
   public static void main(String[] args) {
