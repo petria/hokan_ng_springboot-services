da569298bccbb7f0937ee1527a903ea1ecfd9b60
Petri Airio <petri.j.airio@gmail.com>
2016-08-11 09:51:31 +0300

    Hourly weather
    Identification: -
    Change-Id: I717d435df7000d974295d4f5822fc323f52a815f

--- src/test/java/org/freakz/hokan_ng_springboot/bot/service/IlmatieteenlaitosTest.java
+++ src/test/java/org/freakz/hokan_ng_springboot/bot/service/IlmatieteenlaitosTest.java
@@ -4,8 +4,9 @@ package org.freakz.hokan_ng_springboot.bot.service;
 import org.freakz.hokan_ng_springboot.bot.util.StaticStrings;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 /**
  * Created by Petri Airio on 12.5.2015.
@@ -14,9 +15,25 @@ import org.jsoup.select.Elements;
 public class IlmatieteenlaitosTest {
 
 
-  public static void main(String[] args) throws Exception {
+  public void testShortForecast() throws IOException {
 
-    Document doc = Jsoup.connect("http://en.ilmatieteenlaitos.fi/weather/sastamala").userAgent(StaticStrings.HTTP_USER_AGENT).get();
+    Document doc = Jsoup.connect("http://ilmatieteenlaitos.fi/saa/jyv%C3%A4skyl%C3%A4?forecast=short").userAgent(StaticStrings.HTTP_USER_AGENT).get();
+    Elements times = doc.getElementsByClass("meteogram-times");
+    Elements symbols = doc.getElementsByClass("meteogram-weather-symbols");
+    Elements temperatures = doc.getElementsByClass("meteogram-temperatures");
+
+    String timez = times.get(0).text();
+    String temps = temperatures.get(0).text();
+    int foo = 0;
+
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    IlmatieteenlaitosTest instance = new IlmatieteenlaitosTest();
+    instance.testShortForecast();
+
+/*    Document doc = Jsoup.connect("http://en.ilmatieteenlaitos.fi/weather/sastamala").userAgent(StaticStrings.HTTP_USER_AGENT).get();
     Elements elements = doc.getElementsByClass("time-stamp");
     Element data = elements.get(0);
     String txt = data.text();
@@ -30,7 +47,7 @@ public class IlmatieteenlaitosTest {
       i++;
     }
     int foo = 0;
-    System.out.println(sb.toString());
+    System.out.println(sb.toString());*/
   }
 
 }
