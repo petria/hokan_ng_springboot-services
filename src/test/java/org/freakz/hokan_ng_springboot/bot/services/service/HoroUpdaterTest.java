aed9d4d946a4ba5cd6f958e2e02e3200892a1600
Petri Airio <petri.j.airio@gmail.com>
2017-03-06 17:26:56 +0200

    HoroTest

--- src/test/java/org/freakz/hokan_ng_springboot/bot/services/service/HoroUpdaterTest.java
+++ src/test/java/org/freakz/hokan_ng_springboot/bot/services/service/HoroUpdaterTest.java
@@ -2,9 +2,13 @@ package org.freakz.hokan_ng_springboot.bot.services.service;
 
 import org.freakz.hokan_ng_springboot.bot.common.models.HoroHolder;
 import org.freakz.hokan_ng_springboot.bot.services.updaters.horo.HoroUpdater;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -15,11 +19,17 @@ public class HoroUpdaterTest {
     @Test
     public void testHoroUpdater() throws Exception {
 
-        HoroUpdater updater = new HoroUpdater();
+        HoroUpdater updater = new HoroUpdater() {
+            @Override
+            public Document getDocument() throws IOException {
+                File input = new File("Horoskooppi.htm"); // saved 06.03.2017
+                return Jsoup.parse(input, "ISO-8859-1");
+            }
+        };
         List<HoroHolder> hous = updater.updateIL();
         Assert.assertNotNull(hous);
         HoroHolder holder = hous.get(0);
-        String oinasExpected = "Oinas 21.3.-19.4. Tapaat sattumalta vanhan tuttusi. Huomaat teillä olevan vieläkin paljon yhteistä, vaikka ette ole pitkään aikaan toisianne tavanneetkaan. Juttua ainakin riittää.";
+        String oinasExpected = "OINAS 21.3.-19.4. Arki ei ole nyt sinua varten. Haaveilet jostakin unelmasta, joka liittyy matkailuun. Haluaisit olla omasta elämästäsi enemmän vastuussa, nyt jokin asia ei vain toimi.";
         Assert.assertEquals(oinasExpected, holder.getHoroscopeText());
     }
 
