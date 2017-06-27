a6a4dc256485f0b82a5b119a9911351da54d49f3
Petri Airio <petri.airio@cinia.fi>
2017-06-27 12:36:16 +0300

    Parse lunches

--- src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/lunch/requesthandlers/AntellLunchPlaceHandler.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/lunch/requesthandlers/AntellLunchPlaceHandler.java
@@ -1,13 +1,16 @@
 package org.freakz.hokan_ng_springboot.bot.services.service.lunch.requesthandlers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.freakz.hokan_ng_springboot.bot.common.enums.LunchDay;
 import org.freakz.hokan_ng_springboot.bot.common.enums.LunchPlace;
 import org.freakz.hokan_ng_springboot.bot.common.models.LunchData;
+import org.freakz.hokan_ng_springboot.bot.common.models.LunchMenu;
 import org.freakz.hokan_ng_springboot.bot.common.util.StaticStrings;
 import org.freakz.hokan_ng_springboot.bot.services.service.annotation.LunchPlaceHandler;
 import org.freakz.hokan_ng_springboot.bot.services.service.lunch.LunchRequestHandler;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +18,7 @@ import java.io.IOException;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.time.LocalDateTime;
+import java.util.Iterator;
 
 /**
  * Created by airiope on 26.6.2017.
@@ -23,6 +27,8 @@ import java.time.LocalDateTime;
 @Slf4j
 public class AntellLunchPlaceHandler implements LunchRequestHandler {
 
+    private static String[] markers =
+            {"Maanantai", "Tiistai", "Keskiviikko"};
     @Override
     @LunchPlaceHandler(LunchPlace = LunchPlace.LOUNAS_INFO_ANTELL)
     public void handleLunchPlace(LunchPlace lunchPlaceRequest, LunchData response, LocalDateTime day) {
@@ -41,7 +47,38 @@ public class AntellLunchPlaceHandler implements LunchRequestHandler {
             log.error("Could not fetch lunch from {}", url, e);
             return;
         }
-        Elements rows = doc.getElementsByClass("outer-table");
+        Elements rows = doc.getElementsByTag("tr");
+        int markersIdx = 0;
+        Iterator<Element> iterator = rows.iterator();
+        boolean firstMarkerFound = false;
+        String allText = "";
+        while (iterator.hasNext()) {
+            Element element = iterator.next();
+            String text = element.text();
+            allText += text;
+        }
+        int idx1 = allText.indexOf("Maanantai");
+        int idx2 = allText.indexOf("Tiistai");
+        String maMenu = allText.substring(idx1 + 9, idx2).trim();
+
+        int idx3 = allText.indexOf("Keskiviikko");
+        String tiMenu = allText.substring(idx2 + 7, idx3).trim();
+
+        int idx4 = allText.indexOf("Torstai");
+        String keMenu = allText.substring(idx3 + 11, idx4).trim();
+
+        int idx5 = allText.indexOf("Perjantai");
+        String toMenu = allText.substring(idx4 + 8, idx5).trim();
+
+        int idx6 = allText.indexOf("M maidoton");
+        String peMenu = allText.substring(idx5 + 10, idx6).trim();
+
+        response.getMenu().put(LunchDay.MONDAY, new LunchMenu(maMenu));
+        response.getMenu().put(LunchDay.TUESDAY, new LunchMenu(tiMenu));
+        response.getMenu().put(LunchDay.WEDNESDAY, new LunchMenu(keMenu));
+        response.getMenu().put(LunchDay.THURSDAY, new LunchMenu(toMenu));
+        response.getMenu().put(LunchDay.FRIDAY, new LunchMenu(peMenu));
+
 
         int foo = 0;
     }
