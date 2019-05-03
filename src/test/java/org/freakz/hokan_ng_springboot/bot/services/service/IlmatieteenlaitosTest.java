15f2b192f8d229d375e0a295523206ad4ddec08f
Petri Airio <petri.airio@valamis.com>
2019-05-03 16:01:50 +0300

    Reverse url

--- src/test/java/org/freakz/hokan_ng_springboot/bot/services/service/IlmatieteenlaitosTest.java
+++ src/test/java/org/freakz/hokan_ng_springboot/bot/services/service/IlmatieteenlaitosTest.java
@@ -3,10 +3,13 @@ package org.freakz.hokan_ng_springboot.bot.services.service;
 
 import org.freakz.hokan_ng_springboot.bot.common.util.StaticStrings;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * Created by Petri Airio on 12.5.2015.
@@ -28,10 +31,38 @@ public class IlmatieteenlaitosTest {
 
     }
 
+    public void testAmppari() throws IOException {
+
+
+        String line = "\"Hollywood-tähti Channing Tatum esittelee lihaksiaan suihkussa ilman rihman kiertämää - kuva sai julkkikset sekaisin: \"Rikoit juuri internetin\"\"";
+
+        if (line.matches("\".*\"")) {
+
+            System.out.println("Search   : " + line + "");
+
+            String encode = URLEncoder.encode(line, "UTF-8");
+
+            String url = "https://www.ampparit.com/haku?q=" + encode;
+
+            Document doc = Jsoup.connect(url).userAgent(StaticStrings.HTTP_USER_AGENT).get();
+            Elements elementsByClass = doc.getElementsByClass("news-item-headline");
+            for (Element sub : elementsByClass) {
+                String text = sub.text();
+                if (text.equals(line)) {
+                    Attributes attributes = sub.attributes();
+                    String href = attributes.get("href");
+                    System.out.println("Found Url: " + href);
+                    int foo = 0;
+                }
+            }
+        }
+
+    }
 
     public static void main(String[] args) throws Exception {
         IlmatieteenlaitosTest instance = new IlmatieteenlaitosTest();
-        instance.testShortForecast();
+        instance.testAmppari();
+//        instance.testShortForecast();
 
 /*    Document doc = Jsoup.connect("http://en.ilmatieteenlaitos.fi/weather/sastamala").userAgent(StaticStrings.HTTP_USER_AGENT).get();
     Elements elements = doc.getElementsByClass("time-stamp");
