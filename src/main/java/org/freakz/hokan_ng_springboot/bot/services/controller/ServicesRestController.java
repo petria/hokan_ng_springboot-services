ae4835874cd3a5f9a3e1a89a921494cf1ebe83aa
Petri Airio <petri.j.airio@gmail.com>
2019-05-07 20:43:58 +0300

    torrent

--- src/main/java/org/freakz/hokan_ng_springboot/bot/services/controller/ServicesRestController.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/services/controller/ServicesRestController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@Transactional
+//@Transactional
 public class ServicesRestController {
 
     private static final Logger log = LoggerFactory.getLogger(ServicesRestController.class);
