ca38448975809b2ae8601645df60946dba347449
Petri Airio <petri.j.airio@gmail.com>
2019-11-12 21:26:00 +0200

    SMS send test

--- src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/sms/SMSSenderServiceImpl.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/sms/SMSSenderServiceImpl.java
@@ -19,7 +19,7 @@ public class SMSSenderServiceImpl implements SMSSenderService {
         log.debug("from: {} - to: {} -> {}", from, to, message);
 
         try {
-            String url = String.format("https://api.budgetsms.net/testsms/?Credit=1&username=petria&userid=18436&handle=c11bc789efcd53818aedd7020c2dc5c6&msg=%s&from=%s&to=%s", URLEncoder.encode(message, "UTF-8"), from, to);
+            String url = String.format("https://api.budgetsms.net/sendsms/?Credit=1&username=petria&userid=18436&handle=c11bc789efcd53818aedd7020c2dc5c6&msg=%s&from=%s&to=%s", URLEncoder.encode(message, "UTF-8"), from, to);
             log.debug("URL: {}", url);
             Document doc = Jsoup.connect(url).get();
             Elements body = doc.getElementsByTag("body");
