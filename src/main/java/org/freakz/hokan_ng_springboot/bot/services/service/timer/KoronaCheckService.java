dabe9677eb3c0f1a6725b0af22adb4297c577fd5
Petri Airio <petri.j.airio@gmail.com>
2020-04-07 18:39:03 +0300

    Get Korona JSON data from: https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/processedThlData

--- src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/timer/KoronaCheckService.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/timer/KoronaCheckService.java
@@ -30,40 +30,54 @@ public class KoronaCheckService {
 
     private final ChannelPropertyService channelPropertyService;
     private final JmsSender jmsSender;
+    private final KoronaJSONReader jsonReader;
 
     private Map<String, InfectedStats> ownerToStatsMap = new HashMap<>();
-    private int currentInfected = 0;
-    private int currentHealed = 0;
-    private int currentDead = 0;
+    private Integer dead;
+    private Integer inIcu;
+    private Integer inWard;
+    private Integer total;
 
     @Autowired
-    public KoronaCheckService(ChannelPropertyService channelPropertyService, JmsSender jmsSender) {
+    public KoronaCheckService(ChannelPropertyService channelPropertyService, JmsSender jmsSender, KoronaJSONReader jsonReader) {
         this.channelPropertyService = channelPropertyService;
         this.jmsSender = jmsSender;
+        this.jsonReader = jsonReader;
     }
 
-    private InfectedStats calcInfectedStatsDiffs(int currentInfected, int currentHealed, int currentDead, InfectedStats oldStats) {
+    private InfectedStats calcInfectedStatsDiffs(int currentDead, int currentInIcu, int currentInWard, int currentTotal, InfectedStats oldStats) {
         InfectedStats stats = new InfectedStats();
-        stats.infected = currentInfected;
-        stats.healed = currentHealed;
         stats.dead = currentDead;
+        stats.inIcu = currentInIcu;
+        stats.inWard = currentInWard;
+        stats.total = currentTotal;
 
-        if (stats.infected > oldStats.infected) {
-            stats.infectedDiff = stats.infected - oldStats.infected;
+        if (stats.dead != oldStats.dead) {
+            stats.deadDiff = stats.dead - oldStats.dead;
             stats.d0 = true;
             stats.d1 = true;
         }
-        if (stats.healed > oldStats.healed) {
-            stats.healedDiff = stats.healed - oldStats.healed;
+        if (stats.inIcu != oldStats.inIcu) {
+            stats.inIcuDiff = stats.inIcu - oldStats.inIcu;
             stats.d0 = true;
             stats.d2 = true;
         }
-        if (stats.dead > oldStats.dead) {
-            stats.deadDiff = stats.dead - oldStats.dead;
+        if (stats.inWard != oldStats.inWard) {
+            stats.inWardDiff = stats.inWard - oldStats.inWard;
             stats.d0 = true;
             stats.d3 = true;
         }
+        if (stats.total != oldStats.total) {
+            stats.totalDiff = stats.total - oldStats.total;
+            stats.d0 = true;
+            stats.d4 = true;
+        }
         return stats;
+    }
+
+    @Scheduled(fixedRate = 60 * 1000)
+    public void updateTimer() {
+        updateCurrentStatsJSON();
     }
 
     private String getKoronas() {
@@ -93,22 +107,18 @@ public class KoronaCheckService {
         return true;
     }
 
-    @Scheduled(fixedRate = 10 * 1000)
-    public void updateTimer() {
-        updateCurrentStats();
-    }
-
     @Scheduled(fixedRate = 30 * 60 * 1000)
     public void notifyTimer() {
         InfectedStats oldStats = getInfectedStats("INTERNAL");
-        InfectedStats ns = calcInfectedStatsDiffs(currentInfected, currentHealed, currentDead, oldStats);
+        InfectedStats ns = calcInfectedStatsDiffs(dead, inIcu, inWard, total, oldStats);
         setInfectedStats("INTERNAL", ns);
 
         if (ns.d0) {
-            log.debug("c1: {} - c2: {} - c3: {} -- d1: {} - d2: {} - d3: {}", ns.infected, ns.healed, ns.dead, ns.infectedDiff, ns.healedDiff, ns.deadDiff);
+            log.debug("c1: {} - c2: {} - c3: {} - c4: {} -- d1: {} - d2: {} - d3: {} - d4: {}", ns.dead, ns.inIcu, ns.inWard, ns.total, ns.deadDiff, ns.inIcuDiff, ns.inWardDiff, ns.totalDiff);
             String d1 = "";
             String d2 = "";
             String d3 = "";
+            String d4 = "";
 
             String b1 = "";
             if (ns.d1) {
@@ -122,14 +132,20 @@ public class KoronaCheckService {
             if (ns.d3) {
                 b3 = Colors.BOLD;
             }
+            String b4 = "";
+            if (ns.d4) {
+                b4 = Colors.BOLD;
+            }
 
-            d1 = String.format(" - infected: %d (%s+%d%s)", ns.infected, b1, ns.infectedDiff, b1);
+            d1 = String.format(" - Dead: %d (%s+%d%s)", ns.dead, b1, ns.deadDiff, b1);
 
-//            d2 = String.format(" - healed: %d (%s+%d%s)", ns.healed, b2, ns.healedDiff, b2);
+            d2 = String.format(" - InIcu: %d (%s+%d%s)", ns.inIcu, b2, ns.inIcuDiff, b2);
 
-            d3 = String.format(" - dead: %d (%s+%d%s)", ns.dead, b3, ns.deadDiff, b3);
+            d3 = String.format(" - InWard: %d (%s+%d%s)", ns.inWard, b3, ns.inWardDiff, b3);
 
-            String notify = String.format("Korona update%s%s%s", d1, d2, d3);
+            d4 = String.format(" - total: %d (%s+%d%s)", ns.total, b4, ns.totalDiff, b4);
+
+            String notify = String.format("Korona update%s%s%s%s", d1, d2, d3, d4);
             log.debug("Korona notify: {}", notify);
             sendNotify(notify);
         }
@@ -140,57 +156,63 @@ public class KoronaCheckService {
         String nickId = (String) request.getParameters()[0];
         log.debug("Handle KoronaCMD: {}", nickId);
         InfectedStats oldStats = getInfectedStats(nickId);
-        InfectedStats ns = calcInfectedStatsDiffs(currentInfected, currentHealed, currentDead, oldStats);
+        InfectedStats ns = calcInfectedStatsDiffs(dead, inIcu, inWard, total, oldStats);
         setInfectedStats(nickId, ns);
-        Integer[] ret = {ns.infected, ns.healed, ns.dead, ns.infectedDiff, ns.healedDiff, ns.deadDiff};
+        Integer[] ret = {ns.dead, ns.inIcu, ns.inWard, ns.total, ns.deadDiff, ns.inIcuDiff, ns.inWardDiff, ns.totalDiff};
         response.setResponseData(request.getType().getResponseDataKey(), ret);
-    }
-
-    private void setInfectedStats(String key, InfectedStats newStats) {
-        this.ownerToStatsMap.put(key, newStats);
     }
 
     private InfectedStats getInfectedStats(String key) {
         InfectedStats stats = this.ownerToStatsMap.get(key);
         if (stats == null) {
             stats = new InfectedStats();
-            stats.infected = currentInfected;
-            stats.healed = currentHealed;
-            stats.dead = currentDead;
+            stats.dead = dead;
+            stats.inIcu = inIcu;
+            stats.inWard = inWard;
+            stats.total = total;
             this.ownerToStatsMap.put(key, stats);
         }
         return stats;
     }
 
-    private void updateCurrentStats() {
-        String title = getKoronas();
-        if (title != null) {
-            String[] split = title.split(" ");
-            currentInfected = Integer.parseInt(split[6]);
-            currentHealed = Integer.parseInt(split[10]);
-            currentDead = Integer.parseInt(split[13]);
-//            log.debug("currentInfected: {} - currentHealed: {} -  currentDead: {}", currentInfected, currentHealed, currentDead);
+    private void setInfectedStats(String key, InfectedStats newStats) {
+        this.ownerToStatsMap.put(key, newStats);
+    }
+
+    private void updateCurrentStatsJSON() {
+        Integer[] i = this.jsonReader.readKoronaJSON();
+        if (i != null) {
+            this.dead = i[0];
+            this.inIcu = i[1];
+            this.inWard = i[2];
+            this.total = i[3];
+        } else {
+            log.error("Update failed!");
         }
     }
 
     public void test() {
-        notifyTimer();
-//        updateCurrentStats();
+//        notifyTimer();
+        updateCurrentStatsJSON();
     }
 
-    class InfectedStats {
-        int infected = 0;
-        int healed = 0;
+    static class InfectedStats {
         int dead = 0;
+        int inIcu = 0;
+        int inWard = 0;
+        int total = 0;
 
-        int infectedDiff = 0;
-        int healedDiff = 0;
         int deadDiff = 0;
+        int inIcuDiff = 0;
+        int inWardDiff = 0;
+        int totalDiff = 0;
 
         boolean d0 = false;
+
         boolean d1 = false;
         boolean d2 = false;
         boolean d3 = false;
+        boolean d4 = false;
     }
 
 
