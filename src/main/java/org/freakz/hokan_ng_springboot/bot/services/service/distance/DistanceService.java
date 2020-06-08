75706ec0d10d94cf61d7620aa3cb13cd9f234808
Petri Airio <petri.j.airio@gmail.com>
2020-06-08 10:39:23 +0300

    Hourly data from Foreca

--- src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/distance/DistanceService.java
+++ src/main/java/org/freakz/hokan_ng_springboot/bot/services/service/distance/DistanceService.java
@@ -1,6 +1,10 @@
 package org.freakz.hokan_ng_springboot.bot.services.service.distance;
 
 import lombok.extern.slf4j.Slf4j;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceRequest;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceRequestType;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceResponse2;
+import org.freakz.hokan_ng_springboot.bot.services.service.annotation.ServiceMessageHandler;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -13,16 +17,22 @@ import java.io.IOException;
 public class DistanceService {
 
 
-    public String getDistance(String city1, String city2) {
-        String url = String.format("https://www.vaelimatka.org/%s/%s", city1, city2);
+  @ServiceMessageHandler(ServiceRequestType = ServiceRequestType.MATKA_REQUEST)
+  public void handleServiceRequest(ServiceRequest request, ServiceResponse2<?> response) {
+    int foo = 0;
+  }
 
-        try {
-            Document doc = Jsoup.connect(url).ignoreContentType(true).get();
-            Elements element = doc.getElementsByClass("caption panel-footer");
-            return element.text();
 
-        } catch (IOException e) {
-            log.error("Korona data fetch failed", e);
+  public String getDistance(String city1, String city2) {
+    String url = String.format("https://www.vaelimatka.org/%s/%s", city1, city2);
+
+    try {
+      Document doc = Jsoup.connect(url).ignoreContentType(true).get();
+      Elements element = doc.getElementsByClass("caption panel-footer");
+      return element.text();
+
+    } catch (IOException e) {
+      log.error("Distance data fetch failed", e);
         }
         return null;
     }
--- src/test/java/org/freakz/hokan_ng_springbootservices/IT/IntegrationTest.java
+++ src/test/java/org/freakz/hokan_ng_springbootservices/IT/IntegrationTest.java
@@ -1,6 +1,11 @@
 package org.freakz.hokan_ng_springbootservices.IT;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.freakz.hokan_ng_springboot.bot.common.events.IrcMessageEvent;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceRequest;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceRequestType;
+import org.freakz.hokan_ng_springboot.bot.common.events.ServiceResponse;
+import org.freakz.hokan_ng_springboot.bot.common.util.CommandArgs;
 import org.freakz.hokan_ng_springboot.bot.services.config.RuntimeConfig;
 import org.freakz.hokan_ng_springboot.bot.services.service.distance.DistanceService;
 import org.freakz.hokan_ng_springboot.bot.services.service.horo.HoroFetchServiceImpl;
@@ -8,6 +13,7 @@ import org.freakz.hokan_ng_springboot.bot.services.service.sms.SMSSenderService;
 import org.freakz.hokan_ng_springboot.bot.services.service.sms.SMSSenderServiceImpl;
 import org.freakz.hokan_ng_springboot.bot.services.service.timer.KoronaCheckService;
 import org.freakz.hokan_ng_springboot.bot.services.service.timer.KoronaJSONReader;
+import org.freakz.hokan_ng_springboot.bot.services.service.weather.ForecaWeatherRequestHandler;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -53,13 +59,30 @@ public class IntegrationTest {
 
     @Test
     public void testDistanceService() {
-        DistanceService sut = new DistanceService();
-        String distance1 = sut.getDistance("Oulu", "Kempele");
-        String distance2 = sut.getDistance("Oulu", "Turku");
-        String distance3 = sut.getDistance("Oulu", "Lontoo");
-        String distance4 = sut.getDistance("Oufdflu", "Lontoo");
+      DistanceService sut = new DistanceService();
+      String distance1 = sut.getDistance("Oulu", "Kempele");
+      String distance2 = sut.getDistance("Oulu", "Turku");
+      String distance3 = sut.getDistance("Oulu", "Lontoo");
+      String distance4 = sut.getDistance("Oufdflu", "Lontoo");
 
-        int foo = 0;
+      int foo = 0;
     }
+
+  @Test
+  public void testForecaWeather() {
+    ForecaWeatherRequestHandler sut = new ForecaWeatherRequestHandler();
+    ServiceRequest req = createServiceRequest(ServiceRequestType.FORECA_WEATHER_HOURLY_REQUEST, "!hsaa oulu", "oulu");
+    ServiceResponse resp = new ServiceResponse(req.getType());
+    sut.handleHourlyRequest(req, resp);
+    int foo = 0;
+  }
+
+  private ServiceRequest createServiceRequest(ServiceRequestType type, String message, Object... parameters) {
+    IrcMessageEvent ircEvent = new IrcMessageEvent();
+    ircEvent.setMessage(message);
+    ServiceRequest req = new ServiceRequest(type, ircEvent, new CommandArgs(ircEvent.getMessage()), parameters);
+    return req;
+
+  }
 
 }
